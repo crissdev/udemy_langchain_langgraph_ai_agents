ac000a58202a9fa212089dfc51e9e4a9b9352562
crissdev <criss.trifan@gmail.com>
2026-09-07 21:21:37 +0200

    Add hierarchical agent teams

--- graph_utils.py
+++ graph_utils.py
@@ -4,3 +4,4 @@ from langchain_core.runnables.graph import Graph
 def visualize_graph(graph: Graph, filename: str):
     with open(filename, mode="wb") as f:
         f.write(graph.draw_mermaid_png(background_color="white"))
+        print(f"Graph written to {filename}")
